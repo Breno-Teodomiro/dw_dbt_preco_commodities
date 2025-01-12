7b18e76e37059b01ac9cebe5d2ac121c54ed265e
Breno-Teodomiro <insights.jobs.ia@gmail.com>
2025-01-12 01:22:06 -0300

    feat: adicionar feature dbt inical

--- src/extract_load.py
+++ src/extract_load.py
@@ -10,12 +10,12 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DB_HOST = os.getenv('DB_HOST')
-DB_PORT = os.getenv('DB_PORT')
-DB_NAME = os.getenv('DB_NAME')
-DB_USER = os.getenv('DB_USER')
-DB_PASS = os.getenv('DB_PASS')
-DB_SCHEMA = os.getenv('DB_SCHEMA')
+DB_HOST = os.getenv('DB_PROD_HOST')
+DB_PORT = os.getenv('DB_PROD_PORT')
+DB_NAME = os.getenv('DB_PROD_NAME')
+DB_USER = os.getenv('DB_PROD_USER')
+DB_PASS = os.getenv('DB_PROD_PASS')
+DB_SCHEMA = os.getenv('DB_PROD_SCHEMA')
 
 DATABASE_URL = f'postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
 
